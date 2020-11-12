89219f4bad8827c207f0c00b125bea5f3545d214
smunozc <salvador.munoz0109@gmail.com>
2020-11-12 09:03:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# smunoz-ejercicio-5-ts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/smunoz-ejercicio-5-ts)
---
+++ index.ts
@@ -0,0 +1,24 @@
+// Tipad
+// Para determinar el tipo de los parámetros de la función, debes dar por hecho que los tipos van a ser siempre los de las dos variables que se añaden en la llamada de la linea 20.
+
+let a: number | string;
+let b: string;
+let c: boolean | number;
+
+a = 5;
+b = "texto";
+c = true;
+
+if (Math.random() >= 0.5) {
+  c = 100;
+}
+
+function d(param1: number, param2: number | boolean) {
+  if (typeof param2 === "number") {
+    return param1 + param2;
+  } else {
+    return "el segundo parámetro no es un número";
+  }
+}
+
+console.log(d(a, c));
